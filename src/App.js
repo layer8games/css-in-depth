2ebddf91509ea393a1d91a7a1849f295ca5a7150
Maxwell <maxwell@layer8games.com>
2021-11-05 16:47:34 -0600

    Added app from book. Began work on css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id='page-title' className='title'>Wombat Coffee Roasters</h1>
+        <nav>
+          <ul className='nav'>
+            <li><a href="/">Home</a></li>
+            <li><a href="/coffees">Coffees</a></li>
+            <li><a href="/brewers">Brewers</a></li>
+            <li><a href="/specials" className="featured">Specials</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <p>
+          Be sure to check out
+          <a href="/specials" className="featured">our specials</a>.
+        </p>
+      </main>
     </div>
   );
 }
